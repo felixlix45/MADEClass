472cc4664695830667384c623d216709d4191107
felix <felixlix45@gmail.com>
2019-07-30 14:54:45 +0700

    Code Cleanup

--- app/src/main/java/com/felix/madeclass/database/MovieDatabase.kt
+++ app/src/main/java/com/felix/madeclass/database/MovieDatabase.kt
@@ -1,11 +1,9 @@
 package com.felix.madeclass.database
 
 import android.content.Context
-import android.os.AsyncTask
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.felix.madeclass.dao.MovieDao
 import com.felix.madeclass.model.MovieFavorite
 
@@ -21,32 +19,9 @@ abstract class MovieDatabase : RoomDatabase() {
             if (INSTANCE == null) {
                 INSTANCE = Room.databaseBuilder(context.applicationContext, MovieDatabase::class.java, "movie_database")
                         .fallbackToDestructiveMigration()
-                        .addCallback(callback)
                         .build()
             }
             return INSTANCE
-        }
-
-        private val callback = object : RoomDatabase.Callback() {
-            override fun onCreate(db: SupportSQLiteDatabase) {
-                super.onCreate(db)
-                PopulateDbAsyncTask(INSTANCE).execute()
-            }
-        }
-
-        class PopulateDbAsyncTask(INSTANCE: MovieDatabase?) : AsyncTask<Void, Void, String>() {
-
-            lateinit var movieDao: MovieDao
-
-            fun PopulateDbAsyncTask(db: MovieDatabase) {
-                movieDao = db.movieDao()
-
-            }
-
-            override fun doInBackground(vararg params: Void?): String? {
-                return null
-            }
-
         }
 
         fun destroyDatabase() {
